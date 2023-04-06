7b2746c911e9a16e020bcef76d1f6fdc13bfb262
Serhat <srhtayata@gmail.com>
2023-04-06 21:40:32 +0300

    EventBus.RabbitMQ init

---
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,15 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.RabbitMQ
+{
+    public class EventBusRabbitMQ
+    {
+
+
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/RabbitMQPersistentConnection.cs
@@ -0,0 +1,85 @@
+﻿using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System.Net.Sockets;
+
+namespace EventBus.RabbitMQ
+{
+    public class RabbitMQPersistentConnection : IDisposable
+    {
+        private IConnection connection;
+        private readonly IConnectionFactory connectionFactory;
+        private readonly int retryCount;
+        private object lock_object = new object();
+        private bool _disposed;
+
+        public RabbitMQPersistentConnection(IConnectionFactory connectionFactory, int retryCount = 5)
+        {
+            this.connectionFactory = connectionFactory;
+            this.retryCount = retryCount;
+        }
+
+        public bool IsConnected => connection != null && connection.IsOpen;
+
+        #region CreateModel
+        public IModel CreateModel()
+        {
+            return connection.CreateModel();
+        }
+        #endregion
+        #region TryConnect
+        public bool TryConnect()
+        {
+            lock (lock_object)
+            {
+                var policy = Policy.Handle<SocketException>()
+                             .Or<BrokerUnreachableException>()
+                             .WaitAndRetry(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+                             {
+
+                             });
+
+                policy.Execute(() =>
+                {
+                    connection = connectionFactory.CreateConnection();
+                });
+            }
+
+            if (IsConnected)
+            {
+                connection.ConnectionShutdown += Connection_ConnectionShutdown;
+                connection.CallbackException += Connection_CallbackException;
+                connection.ConnectionBlocked += Connection_ConnectionBlocked;
+                return true;
+            }
+            return false;
+        }
+
+        private void Connection_ConnectionBlocked(object? sender, global::RabbitMQ.Client.Events.ConnectionBlockedEventArgs e)
+        {
+            if (_disposed) return;
+            TryConnect();
+        }
+
+        private void Connection_CallbackException(object? sender, global::RabbitMQ.Client.Events.CallbackExceptionEventArgs e)
+        {
+            if (_disposed) return;
+            TryConnect();
+        }
+
+        private void Connection_ConnectionShutdown(object? sender, ShutdownEventArgs e)
+        {
+            if (_disposed) return;
+            //Log
+            TryConnect();
+        }
+        #endregion
+        #region Dispose
+        public void Dispose()
+        {
+            _disposed = true;
+            connection.Dispose();
+        }
+        #endregion
+    }
+}
