da03273f69f3846bb6d7adc6ed23ea6d86be0f93
Serhat <srhtayata@gmail.com>
2023-04-06 21:13:38 +0300

    EventBusBase completed

--- src/BuildingBlocks/EventBus/EventBus.Base/Events/BaseEventBus.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/BaseEventBus.cs
@@ -1,14 +1,92 @@
 ﻿using EventBus.Base.Abstraction;
+using EventBus.Base.SubManagers;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace EventBus.Base.Events
 {
-    public class BaseEventBus
+    public abstract class BaseEventBus : IEventBus
     {
+        public readonly IServiceProvider ServiceProvider;
+        public readonly IEventBusSubscriptionManager SubsManager;
 
+        public EventBusConfig EventBusConfig { get; set; }
+
+        protected BaseEventBus(IServiceProvider serviceProvider, EventBusConfig eventBusConfig)
+        {
+            ServiceProvider = serviceProvider;
+            this.EventBusConfig = eventBusConfig;
+            SubsManager = new InMemoryEventBusSubscriptionManager(ProcessEventName);
+        }
+
+        #region ProcessEventName
+        public virtual string ProcessEventName(string eventName)
+        {
+            if (EventBusConfig.DeleteEventPrefix)
+                eventName = eventName.TrimStart(EventBusConfig.EventNamePrefix.ToArray());
+
+            if (EventBusConfig.DeleteEventSuffix)
+                eventName = eventName.TrimEnd(EventBusConfig.EventNameSuffix.ToArray());
+
+            return eventName;
+        }
+        #endregion
+        #region GetSubName
+        public virtual string GetSubName(string eventName)
+        {
+            return $"{EventBusConfig.SubscriberClientAppName}.{ProcessEventName(eventName)}";
+        }
+        #endregion
+        #region ProcessEvent
+        public async Task<bool> ProcessEvent(string eventName, string message)
+        {
+            eventName = ProcessEventName(eventName);
+
+            var processed = false;
+
+            if (SubsManager.HasSubscriptionsForEvent(eventName))
+            {
+                var subscriptions = SubsManager.GetHandlersForEvent(eventName);
+
+                using (var scope = ServiceProvider.CreateScope())
+                {
+                    foreach (var subscription in subscriptions)
+                    {
+                        var handler = ServiceProvider.GetService(subscription.HandlerType);
+                        if (handler == null) continue;
+
+                        var eventType = SubsManager.GetEventTypeByName($"{EventBusConfig.EventNamePrefix}{eventName}{EventBusConfig.EventNameSuffix}");
+                        var integrationEvent = JsonSerializer.Deserialize(message, eventType);
+
+                        var concreteType = typeof(IIntegrationEventHandler<>).MakeGenericType(eventType);
+                        await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { integrationEvent });
+                    }
+                }
+
+                processed = true;
+            }
+
+            return processed;
+        }
+        #endregion
+        #region Dispose
+        public virtual void Dispose()
+        {
+            EventBusConfig = null;
+            SubsManager.Clear();
+        }
+        #endregion
+
+        public abstract void Publish(IntegrationEvent @event);
+
+        public abstract void Subscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+        public abstract void UnSubscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
     }
 }
--- src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace EventBus.Base.Events
 {
     public class IntegrationEvent
     {
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+        [JsonConstructor]
+        public IntegrationEvent(Guid id, DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+
+        [JsonProperty]
+        public Guid Id { get; private set; }
+
+        [JsonProperty]
+        public DateTime CreatedDate { get; private set; }
     }
 }
