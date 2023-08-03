05426089a2fa5ace2aee3f586d999d9589f081c8
Serhat <srhtayata@gmail.com>
2023-08-03 18:31:52 +0300

    CatalogService.Api controllers completed

--- src/Services/CatalogService/CatalogService.Api/Controllers/FeatureController.cs
+++ src/Services/CatalogService/CatalogService.Api/Controllers/FeatureController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CatalogService.Api.Entities;
+using CatalogService.Api.Models.Base.Concrete;
+using CatalogService.Api.Models.FeatureModels;
+using CatalogService.Api.Services.Base.Abstract;
+using CatalogService.Api.Utilities.Results;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace CatalogService.Api.Controllers
 {
@@ -7,6 +13,165 @@ namespace CatalogService.Api.Controllers
     [ApiController]
     public class FeatureController : ControllerBase
     {
+        private readonly IFeatureService _featureService;
 
+        public FeatureController(
+            IFeatureService featureService)
+        {
+            _featureService = featureService;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> AddAsync([FromBody] FeatureAddModel model)
+        {
+            var result = await _featureService.AddAsync(model);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> UpdateAsync([FromBody] FeatureUpdateModel model)
+        {
+            var result = await _featureService.UpdateAsync(model);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> DeleteAsync([FromBody] IntModel model)
+        {
+            var result = await _featureService.DeleteAsync(model);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("add-productfeature")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> AddProductFeatureAsync([FromBody] ProductFeatureModel model)
+        {
+            var result = await _featureService.AddProductFeatureAsync(model);
+            return Ok(result);
+        }
+
+
+        [HttpDelete]
+        [Route("delete-productfeature")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> DeleteProductFeatureAsync([FromBody] ProductFeatureModel model)
+        {
+            var result = await _featureService.DeleteProductFeatureAsync(model);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("add-productfeature-property")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> AddProductFeaturePropertyAsync([FromBody] ProductFeaturePropertyAddModel model)
+        {
+            var result = await _featureService.AddProductFeaturePropertyAsync(model);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        [Route("update-productfeature-property")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> UpdateProductFeaturePropertyAsync([FromBody] ProductFeaturePropertyUpdateModel model)
+        {
+            var result = await _featureService.UpdateProductFeaturePropertyAsync(model);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("delete-productfeature-property")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> DeleteProductFeaturePropertyAsync([FromBody] IntModel model)
+        {
+            var result = await _featureService.DeleteProductFeaturePropertyAsync(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("get")]
+        [ProducesResponseType(typeof(DataResult<FeatureModel>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAsync([FromBody] IntModel model)
+        {
+            var result = await _featureService.GetAsync(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<FeatureModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var result = await _featureService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall-paged")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<FeatureModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllPagedAsync([FromBody] PagingModel model)
+        {
+            var result = await _featureService.GetAllPagedAsync(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall-features-byproductid")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<FeatureModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllFeaturesByProductId([FromBody] IntModel model)
+        {
+            var result = await _featureService.GetAllFeaturesByProductId(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall-featureproperties")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<ProductFeaturePropertyModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllFeatureProperties([FromBody] ProductFeatureModel model)
+        {
+            var result = await _featureService.GetAllFeatureProperties(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall-featureproperties-byproductfeatureid")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<ProductFeaturePropertyModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllFeaturePropertiesByProductFeatureId([FromBody] IntModel model)
+        {
+            var result = await _featureService.GetAllFeaturePropertiesByProductFeatureId(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall-features-byproductcode")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<FeatureModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllFeaturesByProductCode([FromBody] StringModel model)
+        {
+            var result = await _featureService.GetAllFeaturesByProductCode(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("get-featureproducts")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<Product>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetFeatureProducts([FromBody] IntModel model)
+        {
+            var result = await _featureService.GetFeatureProducts(model);
+            return Ok(result);
+        }
     }
 }
--- src/Services/CatalogService/CatalogService.Api/Controllers/ProductController.cs
+++ src/Services/CatalogService/CatalogService.Api/Controllers/ProductController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CatalogService.Api.Models.Base.Concrete;
+using CatalogService.Api.Models.ProductModels;
+using CatalogService.Api.Services.Base.Abstract;
+using CatalogService.Api.Utilities.Results;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace CatalogService.Api.Controllers
 {
@@ -7,5 +12,105 @@ namespace CatalogService.Api.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+
+        public ProductController(
+            IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> AddAsync([FromBody] ProductAddModel model)
+        {
+            var result = await _productService.AddAsync(model);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> UpdateAsync([FromBody] ProductUpdateModel model)
+        {
+            var result = await _productService.UpdateAsync(model);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        [ProducesResponseType(typeof(Result), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(Result))]
+        public async Task<IActionResult> DeleteAsync([FromBody] IntModel model)
+        {
+            var result = await _productService.DeleteAsync(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("get")]
+        [ProducesResponseType(typeof(DataResult<ProductModel>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAsync([FromBody] IntModel model)
+        {
+            var result = await _productService.GetAsync(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<ProductModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var result = await _productService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall-paged")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<ProductModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllPagedAsync([FromBody] PagingModel model)
+        {
+            var result = await _productService.GetAllPagedAsync(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("get-byproductcode")]
+        [ProducesResponseType(typeof(DataResult<ProductModel>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetByProductCodeAsync([FromBody] StringModel model)
+        {
+            var result = await _productService.GetByProductCodeAsync(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall-betweenprices")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<ProductModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllBetweenPricesAsync([FromBody] PriceBetweenModel model)
+        {
+            var result = await _productService.GetAllBetweenPricesAsync(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall-bybrandid")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<ProductModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllByBrandIdAsync([FromBody] IntModel model)
+        {
+            var result = await _productService.GetAllByBrandIdAsync(model);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("getall-byproducttypeid")]
+        [ProducesResponseType(typeof(DataResult<IReadOnlyList<ProductModel>>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllByProductTypeIdAsync([FromBody] IntModel model)
+        {
+            var result = await _productService.GetAllByProductTypeIdAsync(model);
+            return Ok(result);
+        }
     }
 }
