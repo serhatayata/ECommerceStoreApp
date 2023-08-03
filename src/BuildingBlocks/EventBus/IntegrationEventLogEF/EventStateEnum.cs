02b5fd716a8310d26d16d0aef89c09f50ae56424
Serhat <srhtayata@gmail.com>
2023-08-03 18:48:16 +0300

    EventBus event log mechanism added

---
+++ src/BuildingBlocks/EventBus/IntegrationEventLogEF/EventStateEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationEventLogEF
+{
+    public enum EventStateEnum
+    {
+        NotPublished = 0,
+        InProgress = 1,
+        Published = 2,
+        PublishedFailed = 3
+    }
+}
