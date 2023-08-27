c215efeae0d09d83ba3b641a22505de9896dbda2
Serhat <srhtayata@gmail.com>
2023-08-27 12:29:41 +0300

    ApiGateway CatalogController init, will be completed

---
+++ src/ApiGateways/WebApiGateway/Web.ApiGateway/Controllers/CatalogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.ApiGateway.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[EnableCors("CorsPolicy")]
+public class CatalogController : ControllerBase
+{
+
+}
