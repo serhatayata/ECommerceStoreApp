e58796e51381fb1cf9b777afeaae736ae0c51aca
Serhat <srhtayata@gmail.com>
2023-10-16 21:49:00 +0300

    NotificationService.Api init

---
+++ src/Services/NotificationService/NotificationService.Api/Controllers/EmailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+
+    }
+}
---
+++ src/Services/NotificationService/NotificationService.Api/DependencyResolvers/Autofac/AutofacBusinessModel.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace NotificationService.Api.DependencyResolvers.Autofac;
+
+public class AutofacBusinessModel : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        //builder.RegisterType<HealthCheckDiagnosticService>().As<IHealthCheckDiagnosticService>().InstancePerDependency();
+        //builder.RegisterType<LocalizationService>().As<ILocalizationService>().InstancePerDependency();
+    }
+}
