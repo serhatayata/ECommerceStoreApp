bc6e41e7f8574f63e812c83ec665f0ae86380086
Serhat <srhtayata@gmail.com>
2023-11-03 21:02:33 +0300

    Order-payment-stock services init

---
+++ src/Services/OrderService/OrderService.Api/Configurations/Installers/ServiceInstallers/StartupDIServiceInstaller.cs
@@ -0,0 +1,15 @@
+﻿namespace OrderService.Api.Configurations.Installers.ServiceInstallers;
+
+public class StartupDIServiceInstaller : IServiceInstaller
+{
+    public void Install(IServiceCollection services, IConfiguration configuration, IWebHostEnvironment hostEnvironment)
+    {
+        //services.AddScoped<IClientCredentialsTokenService, ClientCredentialsTokenService>();
+        //services.AddSingleton<IRedisService, RedisService>();
+        //services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig =>
+        //{
+        //    var address = configuration["ConsulConfig:Address"];
+        //    consulConfig.Address = new Uri(address);
+        //}));
+    }
+}
