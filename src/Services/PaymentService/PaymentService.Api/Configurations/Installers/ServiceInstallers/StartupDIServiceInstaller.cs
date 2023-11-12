c50c414408e40c028c53d8fd946a62240dd72756
Serhat <srhtayata@gmail.com>
2023-11-12 19:36:19 +0300

    PaymentService.Api init

---
+++ src/Services/PaymentService/PaymentService.Api/Configurations/Installers/ServiceInstallers/StartupDIServiceInstaller.cs
@@ -0,0 +1,9 @@
+﻿namespace PaymentService.Api.Configurations.Installers.ServiceInstallers;
+
+public class StartupDIServiceInstaller : IServiceInstaller
+{
+    public void Install(IServiceCollection services, IConfiguration configuration, IWebHostEnvironment hostEnvironment)
+    {
+        throw new NotImplementedException();
+    }
+}
