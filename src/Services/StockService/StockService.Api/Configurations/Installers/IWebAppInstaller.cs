87b93da9c51f6310a1287fdc0122173e5022b435
Serhat <srhtayata@gmail.com>
2023-11-19 12:51:20 +0300

    StockService.Api health check added

---
+++ src/Services/StockService/StockService.Api/Configurations/Installers/IWebAppInstaller.cs
@@ -0,0 +1,6 @@
+﻿namespace StockService.Api.Configurations.Installers;
+
+public interface IWebAppInstaller
+{
+    void Install(IApplicationBuilder app, IHostApplicationLifetime lifeTime, IConfiguration configuration);
+}
