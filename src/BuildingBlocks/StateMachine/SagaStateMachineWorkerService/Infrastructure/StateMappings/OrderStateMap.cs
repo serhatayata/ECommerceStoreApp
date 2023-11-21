1dcb4ce7ff7aa41c67eb513288cf362106cd4929
Serhat <srhtayata@gmail.com>
2023-11-21 21:31:07 +0300

    StateMachine OrderStateDbContext added

---
+++ src/BuildingBlocks/StateMachine/SagaStateMachineWorkerService/Infrastructure/StateMappings/OrderStateMap.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SagaStateMachineWorkerService.Models;
+
+namespace SagaStateMachineWorkerService.Infrastructure.StateMappings;
+
+public class OrderStateMap : SagaClassMap<OrderStateInstance>
+{
+    protected override void Configure(EntityTypeBuilder<OrderStateInstance> entity, ModelBuilder model)
+    {
+        entity.Property(o => o.BuyerId).HasMaxLength(256);
+        entity.Property(o => o.BuyerId).IsRequired(true);
+
+        entity.Property(o => o.TotalPrice).HasPrecision(10, 2);
+
+        entity.Property(o => o.OrderId).IsRequired(true);
+    }
+}
