0552ebf4aa301315215c4806f273ba8fe2a90ea2
Serhat <srhtayata@gmail.com>
2024-03-16 17:57:43 +0300

    Localization service dockerize

--- src/Services/LocalizationService/LocalizationService.Api/Configurations/Installers/ServiceInstallers/DbContextServiceInstaller.cs
+++ src/Services/LocalizationService/LocalizationService.Api/Configurations/Installers/ServiceInstallers/DbContextServiceInstaller.cs
@@ -12,6 +12,11 @@ public class DbContextServiceInstaller : IServiceInstaller
         var assembly = typeof(Program).Assembly.GetName().Name;
 
         string defaultConnString = configuration.GetConnectionString("DefaultConnection");
-        services.AddDbContext<ILocalizationDbContext, LocalizationDbContext>(options => options.UseSqlServer(defaultConnString, b => b.MigrationsAssembly(assembly)), ServiceLifetime.Transient);
+        services.AddDbContext<ILocalizationDbContext, LocalizationDbContext>(options => options.UseSqlServer(defaultConnString, b => b.MigrationsAssembly(assembly)), ServiceLifetime.Scoped);
+
+        var serviceProvider = services.BuildServiceProvider();
+        var context = serviceProvider.GetRequiredService<LocalizationDbContext>();
+
+        _ = context.Database.EnsureCreated();
     }
 }
