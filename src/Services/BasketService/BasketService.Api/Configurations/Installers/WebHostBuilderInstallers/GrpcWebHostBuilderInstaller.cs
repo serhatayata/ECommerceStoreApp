34006833a7714c65b4020bb7333f661697bc329b
Serhat <srhtayata@gmail.com>
2024-03-20 00:06:34 +0300

    BasketService.Api dockerize

--- src/Services/BasketService/BasketService.Api/Configurations/Installers/WebHostBuilderInstallers/GrpcWebHostBuilderInstaller.cs
+++ src/Services/BasketService/BasketService.Api/Configurations/Installers/WebHostBuilderInstallers/GrpcWebHostBuilderInstaller.cs
@@ -7,42 +7,24 @@ public class GrpcWebHostBuilderInstaller : IWebHostBuilderInstaller
 {
     public void Install(ConfigureWebHostBuilder builder, IWebHostEnvironment hostEnv, IConfiguration configuration)
     {
-        if (hostEnv.IsProduction())
+        builder.UseKestrel(options =>
         {
-            builder.UseKestrel(options =>
+            var ports = GetDefinedPorts(configuration);
+            options.ListenAnyIP(ports.grpcPort, listenOptions =>
             {
-                var ports = GetDefinedPorts(configuration);
-                options.Listen(IPAddress.Any, ports.grpcPort, listenOptions =>
-                {
-                    listenOptions.Protocols = HttpProtocols.Http2;
-                });
-                options.Listen(IPAddress.Any, ports.httpPort, listenOptions =>
-                {
-                    listenOptions.Protocols = HttpProtocols.Http1;
-                });
+                listenOptions.Protocols = HttpProtocols.Http2;
             });
-        }
-        else
-        {
-            builder.UseKestrel(options =>
+            options.ListenAnyIP(ports.httpPort, listenOptions =>
             {
-                var ports = GetDefinedPorts(configuration);
-                options.ListenLocalhost(ports.grpcPort, listenOptions =>
-                {
-                    listenOptions.Protocols = HttpProtocols.Http2;
-                });
-                options.ListenLocalhost(ports.httpPort, listenOptions =>
-                {
-                    listenOptions.Protocols = HttpProtocols.Http1;
-                });
+                listenOptions.Protocols = HttpProtocols.Http1;
             });
-        }
+        });
     }
 
     static (int httpPort, int grpcPort) GetDefinedPorts(IConfiguration config)
     {
         var grpcPort = config.GetValue("GRPC_PORT", 7005);
-        var port = config.GetValue("PORT", 5005);
+        var port = config.GetValue("PORT", 6005);
         return (port, grpcPort);
     }
 }
